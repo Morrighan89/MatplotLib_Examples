527413e0409893e3c340edea0033c34a693cd940
Morrighan89 <riccardo.ferrero.math@gmail.com>
2016-07-20 11:59:28 +0200

    second commit

---
+++ data_smoothing.py
@@ -0,0 +1,18 @@
+from numpy import loadtxt
+from scipy.ndimage.filters import gaussian_filter
+from matplotlib.pyplot import contour, show
+
+sigma = 0.7 # this depends on how noisy your data is, play with it!
+data = loadtxt('data.txt')
+data = gaussian_filter(data, sigma)
+contour(data)
+show()
+from numpy import loadtxt
+from scipy.ndimage.filters import gaussian_filter
+from matplotlib.pyplot import contour, show
+
+sigma = 0.7 # this depends on how noisy your data is, play with it!
+data = loadtxt('data.txt')
+data = gaussian_filter(data, sigma)
+contour(data)
+show()
