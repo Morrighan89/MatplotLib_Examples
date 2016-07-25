b59b472720d07fd3f09c48531c80ef4cde4410ce
Enrico Simonetto <e.simonetto@magmodel.lcl>
2016-07-25 12:33:05 +0200

    plot3d

--- prova.py
+++ prova.py
@@ -1,3 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+
+(X,Y,Z) = np.loadtxt('prova.DAT')
+
+print(X)
+print(X.size)
+print(Y)
+print(Y.size)
+print(Z)
+print(Z.size)
+Axes3D.plot_surface(X, Y, Z)
