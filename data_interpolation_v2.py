0e42841e20b61e2bb877068e5c1ee1d22fe98d41
Morrighan89 <riccardo.ferrero.math@gmail.com>
2016-07-26 14:33:25 +0200

    prova

--- data_interpolation_v2.py
+++ data_interpolation_v2.py
@@ -2,9 +2,9 @@ import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
-
-def func(x, y):
-     return x*(1-x)*np.cos(4*np.pi*x) * np.sin(4*np.pi*y**2)**2
+origin = 'lower'
+#def func(x, y):
+#     return x*(1-x)*np.cos(4*np.pi*x) * np.sin(4*np.pi*y**2)**2
 
 
 grid_x, grid_y = np.mgrid[200:650:100j, 10:30:100j]
@@ -14,6 +14,14 @@ values=(data[:,2])
 
 #values = np.loadtxt('DATI2.DAT')
 grid_z0 = griddata(points, values, (grid_x, grid_y), method='cubic')
-print(grid_z0)
-plt.contourf(grid_x,grid_y,grid_z0)
+
+CS=plt.contourf(grid_x,grid_y,grid_z0,100)
+plt.rcParams['contour.negative_linestyle'] = 'solid'
+CS2 = plt.contour(CS, levels=CS.levels[::10],
+                  colors='k',
+                  origin=origin,
+                  hold='on')
+plt.clabel(CS2, fontsize=9, inline=1)
+#plt.axis([200, 650, 10, 30])
+plt.colorbar(CS)
 plt.show()
